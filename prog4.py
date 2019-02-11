d21955cfd86b4ac9deb24ac574a73a3eb32fd737
pinjago <philipp.schellenberg.biz@gmail.com>
2019-02-11 20:18:24 +0100

    more code

---
+++ prog4.py
@@ -0,0 +1,16 @@
+sampleList = [1,2,3,4,5,6,7,8]
+colorlist = ["red","green","yellow"]
+print (sampleList[0])
+
+print("all")
+
+for a in sampleList:
+    print(a)
+
+for b in colorlist:
+    print(b)
+
+    colorlist = ["blue"]
+
+    for c in colorlist:
+        print(c)
